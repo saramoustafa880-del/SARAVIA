2171f160b324d0ec3be34bbf3d5f7c883ebde222
saramoustafa880-del <sselmabarakat880@gmail.com>
2026-04-06 05:00:59 +0300

    Add files via upload

---
+++ sara-via/apps/server/jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  },
+  collectCoverageFrom: ['src/**/*.ts']
+};
+
+export default config;
